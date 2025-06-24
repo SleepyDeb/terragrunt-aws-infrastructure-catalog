138799510c7442d5d908e31eddc4f0a0e96a08b6
Alessio DM <adimaria@tai.it>
2025-06-24 17:36:53 +0200

    table 123

--- modules/dynamodb-app-table/main.tf
+++ modules/dynamodb-app-table/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "table" {
-  name         = "${var.deployment_name}-table"
+  name         = "${var.deployment_name}-123-table"
   billing_mode = var.billing_mode
   hash_key     = var.hash_key
   attribute {
