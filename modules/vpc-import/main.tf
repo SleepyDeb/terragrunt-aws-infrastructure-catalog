b613a94250a0829cf9e2bf7b08c1020a7eef2867
Alessio Di Maria <alessio.dimaria@gmail.com>
2025-09-14 01:25:31 +0000

    creating vpc import module

---
+++ modules/vpc-import/main.tf
@@ -0,0 +1,45 @@
+# INPUT VARIABLES
+variable "vpc_id" {
+  description = "The VPC ID to import"
+  type        = string
+}
+
+# RESOURCE DEFINITIONS
+# reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc
+data "aws_vpc" "vpc" {
+  id = var.vpc_id
+}
+
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+data "aws_subnet" "subnets" {
+  for_each = toset(data.aws_subnets.subnets.ids)
+  id       = each.value
+}
+
+# OUTPUTS
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_name" {
+  description = "The Name of the VPC"
+  value       = module.vpc.name
+}
+
+output "vpc_subnet_ids" {
+  description = "List of all Subnet IDs"
+  value       = module.vpc_subnets.subnets.ids
+}
+
+output "vpc_subnets" {
+  description = "List of all Subnets"
+  value       = module.vpc_subnet.subnets
+}
+
